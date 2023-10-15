9e40fbbf15ef9cff64ad6e07aa9f700323447e0e
George Vazquez <george.h.vazquez@gmail.com>
2023-10-15 15:52:23 -0400

    remove unnecessary variables

--- Source/hurdle/Cube.cpp
+++ Source/hurdle/Cube.cpp
@@ -1,36 +1,28 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "Cube.h"
 
 // Sets default values
 ACube::ACube()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
-	Value = 100;
-
 }
 
 // Called when the game starts or when spawned
 void ACube::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
 // Called every frame
 void ACube::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
 // Called to bind functionality to input
-void ACube::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+void ACube::SetupPlayerInputComponent(UInputComponent *PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
 }
-
